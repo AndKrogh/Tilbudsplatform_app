5a2085e447cbeed7bacb97cd7584cda160899bff
AndKrogh <anders.krogh99@gmail.com>
2025-01-13 22:25:36 +0100

    trying to download as excel file

---
+++ TilbudsPlatform/wwwroot/js/DownloadExcelFile.js
@@ -0,0 +1,8 @@
+﻿function downloadFile(filename, filecontent) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + filecontent;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
